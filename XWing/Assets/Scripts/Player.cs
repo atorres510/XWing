d650ee58af66bb017f4e13048594b42e7d6136a5
atorres510 <torre157@umn.edu>
2016-05-14 23:27:27 -0500

    Added PUN and initial network manager

---
+++ XWing/Assets/Scripts/Player.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+    public int playerID = 0;
+
+
+
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
