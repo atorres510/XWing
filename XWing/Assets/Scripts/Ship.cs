713bcdfe22d6388430662b66a76da0da52def98a
rmdt318 <Spirit318@charter.net>
2016-05-15 18:14:28 -0700

    Ship movement progress.

--- XWing/Assets/Scripts/Ship.cs
+++ XWing/Assets/Scripts/Ship.cs
@@ -14,19 +14,83 @@ public class Ship : MonoBehaviour
     [SerializeField] int NumberOfFocus;
 
     [SerializeField] Maneuver[] ManeuverDial;
-    
+
+    private float shipSpeed = 3.0f;
+    public Transform target;
+    float timeCounter = 0;
+    float degreeCounter = 0;
+    bool inCircularManeuver = false;
 
     // Use this for initialization
     void Start()
     {
-	
-	}
+        target.position = transform.position;
+
+    }
 	
 	// Update is called once per frame
 	void Update()
     {
-	
-	}
+        MoveCommand();
+        CircularManeuver(1, 1f, 0, 45f, true);
+    }
+
+    /* Radius:
+     * Xoffset:
+     * Yoffset:
+     * Degree:
+     */
+    void CircularManeuver(float radius, float xoffset, float yoffset, float degree, bool isLeft)
+    {
+        if(isLeft)
+        {
+            if (inCircularManeuver)
+            {
+                timeCounter += Time.deltaTime;
+                degreeCounter += 1f;
+                target.Rotate(0, 0, 1);
+                float x = Mathf.Cos(timeCounter) * radius - xoffset;
+                float y = Mathf.Sin(timeCounter) * radius + yoffset;
+                float z = 0;
+                target.position = new Vector3(x, y, z);
+            }
+        }
+        else
+        {
+            if (inCircularManeuver)
+            {
+                timeCounter += Time.deltaTime;
+                degreeCounter += 1f;
+                target.Rotate(0, 0, -1);
+                float x = -Mathf.Cos(timeCounter) * radius + xoffset;
+                float y = Mathf.Sin(timeCounter) * radius + yoffset;
+                float z = 0;
+                target.position = new Vector3(x, y, z);
+            }
+        }
+        
+        if(degree <= degreeCounter)
+        {
+            inCircularManeuver = false;
+            degreeCounter = 0;
+        }
+    }
+
+    void MoveCommand()
+    {
+        if(Input.GetKey(KeyCode.W))
+        {
+            inCircularManeuver = true;
+        }
+        transform.rotation = target.rotation;
+        transform.position = Vector3.MoveTowards(transform.position, target.position, shipSpeed * Time.deltaTime);
+        
+    }
+
+    void StraightManeuver(float distance)
+    {
+        target.position = new Vector3(transform.position.x, transform.position.y + distance, transform.position.z);
+    }
 
     void AttackCommand()
     {
