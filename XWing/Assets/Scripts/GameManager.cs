7a8ecdf9f36ccc727185f846b0fbd7aec3889a84
atorres510 <torre157@umn.edu>
2016-05-15 20:37:18 -0500

    Players now able to 'ready up'

--- XWing/Assets/Scripts/GameManager.cs
+++ XWing/Assets/Scripts/GameManager.cs
@@ -7,9 +7,58 @@ public class GameManager : MonoBehaviour {
     Player myPlayer;
     List<Player> playerList = new List<Player>();
 
+    bool gameOn = false;
+    int previousPhase = 0;
 
+    public int currentPhase;
+    int correctedCurrentPhase;
+
+
+
+
+    //holds local photonview component
     PhotonView photonView;
 
+    //holds parent UI objects that hold other relevent UI objects.  used to activate/deactivate groups of elements more easily
+    public GameObject[] UIParentObjects; //0 - static elements, 1 - setup, 2 - planning, 3 - activation, 4 - combat, 5 - resolution
+
+    void Start()
+    {
+
+        photonView = gameObject.GetComponent<PhotonView>();
+
+        if (PhotonNetwork.offlineMode)
+        {
+
+            //do singleplayer stuff
+
+
+        }
+
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (gameOn) {
+
+            ChangePhases();
+            PhotonViewUpdate();
+
+        }
+
+
+
+
+    }
+
+    #region SetUpMethods
+    //holds methods that set up the board, GM member variables, and players
+   
     //called in network manager when online, called in Start() when offline
     public void SetMyPlayer(Player p) {
 
@@ -43,11 +92,117 @@ public class GameManager : MonoBehaviour {
 
         }
 
+        StartGame();
+
+
+    }
+
+    #endregion
+
+    #region UI Methods
+
+    public void EnableParentUIObject(int phase) {
+
+        UIParentObjects[phase].gameObject.SetActive(true);
+
+
+    }
+
+    public void DisableParentUIObject(int phase)
+    {
+
+        UIParentObjects[phase].gameObject.SetActive(false);
+
 
     }
 
 
 
+    public void ReadyToStartButton() {
+
+        myPlayer.isCommitted = true;
+
+        if (AreAllOtherClientsCommitted()) {
+            Debug.Log("Changing from Phase " + currentPhase + " to Phase " + (currentPhase + 1));
+            currentPhase++;
+
+        }
+
+    }
+
+
+
+
+
+
+    #endregion
+
+    #region Client to Client Methods
+    //used for communication between players, networked or local
+    bool AreAllOtherClientsCommitted() {
+
+        foreach (Player p in playerList) {
+
+            if (!p.isCommitted) {
+
+                return false;
+
+            }
+
+
+
+        }
+
+        return true;
+        
+    }
+
+
+    void ChangePhases() {
+
+        if (currentPhase != previousPhase)
+        {
+            DisableParentUIObject(previousPhase);
+            EnableParentUIObject(currentPhase);
+            previousPhase = currentPhase;
+        }
+
+        else {
+
+            
+
+        }
+
+
+
+    }
+
+
+
+
+
+    #endregion
+
+    #region GameMethods
+    //hold phases of the game, such as planning, activation, and combat along with rules and other game data
+
+    void StartGame() {
+
+        //Phase 1 - SetUp
+        currentPhase = 1;
+        previousPhase = 1;
+        EnableParentUIObject(currentPhase);
+        gameOn = true;
+
+
+    }
+
+
+
+    #endregion
+
+    #region PUNMethods
+    //Holds any method directly using PUN source code
 
     //used by photon view to stream data between watched objects
     void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
@@ -56,36 +211,38 @@ public class GameManager : MonoBehaviour {
         if (stream.isWriting)
         {
 
+            stream.SendNext(currentPhase);
 
         }
 
         else {
 
-
-
-        }
-
-    }
-
-
-    void Start() {
-
-        photonView = gameObject.GetComponent<PhotonView>();
-
-        if (PhotonNetwork.offlineMode) {
-
-            //do singleplayer stuff
-
+            this.correctedCurrentPhase = (int)stream.ReceiveNext();
 
         }
 
     }
 
+    //used to stream info through photonserialize view.  called in Update()
+    void PhotonViewUpdate()
+    {
+
+        if (!photonView.isMine)
+        {
+
+            currentPhase = correctedCurrentPhase;
+
+
+        }
 
 
 
-    // Update is called once per frame
-    void Update () {
-	
-	}
+
+    }
+
+
+    #endregion
+
+
+
 }
--- XWing/Assets/Scripts/Player.cs
+++ XWing/Assets/Scripts/Player.cs
@@ -5,9 +5,43 @@ public class Player : MonoBehaviour {
 
     public int playerID = 0;
 
+    PhotonView photonView;
+
+    public bool isCommitted = false;
+    bool correctedIsCommitted = false;
 
 
 
+    void Start()
+    {
+
+        photonView = gameObject.GetComponent<PhotonView>();
+
+        if (PhotonNetwork.offlineMode)
+        {
+
+            //do singleplayer stuff
+
+
+        }
+
+    }
+
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        PhotonViewUpdate();
+
+
+    }
+
+
+
+    #region Set/Get Methods
     [PunRPC]
     public void SetPlayerID (int id){
 
@@ -21,7 +55,9 @@ public class Player : MonoBehaviour {
 
     }
 
+    #endregion
 
+    #region PUN Methods
     //used by photon view to stream data between watched objects
     void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
     {
@@ -29,15 +65,39 @@ public class Player : MonoBehaviour {
         if (stream.isWriting)
         {
 
-         
+            stream.SendNext(isCommitted);
+
         }
 
         else {
 
-          
+            this.correctedIsCommitted = (bool)stream.ReceiveNext();
 
         }
 
     }
-    
+
+    //used to stream info through photonserialize view.  called in Update()
+    void PhotonViewUpdate()
+    {
+
+        if (!photonView.isMine)
+        {
+
+            isCommitted = correctedIsCommitted;
+
+
+        }
+
+
+
+
+    }
+
+
+    #endregion
+
+
+
+
 }
